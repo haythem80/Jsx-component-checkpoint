54081516cff027fb6815cddb9a063d7c31773779
haythem80 <haythemmlik022@gmail.com>
2024-02-24 14:17:53 +0100

    jsx

--- src/component/Navebare.jsx
+++ src/component/Navebare.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Container, Form, Navbar } from 'react-bootstrap'
+import { Button, Container, Form, NavDropdown, Navbar } from 'react-bootstrap'
 import { CiSearch } from "react-icons/ci";
 import { HiShoppingCart } from "react-icons/hi";
 import './Navebare.css';
@@ -7,8 +7,8 @@ import { MdOutlineShoppingCart } from "react-icons/md";
 
 const Navebare = () => {
   return (
-    <div className='nav'>
-        <Navbar expand="lg" className="bg-body-tertiary">
+    <div className='nav' >
+        <Navbar expand="lg" className="bg-body-tertiary" >
       <Container fluid>
         <Navbar.Brand href="#" className='name' >
             Shopping 
@@ -26,7 +26,13 @@ const Navebare = () => {
             <Button variant="outline-success">
             <CiSearch  id="Ciserach"/>
             </Button>
+          <NavDropdown title="Category" id="basic-nav-dropdown">
+              <NavDropdown.Item id = "dropdown" href="#action/3.1">Electronics</NavDropdown.Item>
+              <NavDropdown.Item id = "dropdown" href="#action/3.2">Fashion</NavDropdown.Item>
+              <NavDropdown.Item id = "dropdown" href="#action/3.2">sport</NavDropdown.Item>
+            </NavDropdown>
             <MdOutlineShoppingCart  id ="linecard"/>
+            <h4> cart </h4>
           </Form>
         </Navbar.Collapse>
       </Container>
